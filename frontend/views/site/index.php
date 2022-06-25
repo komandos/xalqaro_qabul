37eb52b8dbce2d1ebe040bc9efdc47a3355cb78b
DilshodDev <allashkurovdilshod@gmail.com>
2022-06-25 13:42:23 +0500

    index page change

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -22,10 +22,16 @@ $this->title = Yii::t('app', 'UrSU Vacancy');
 ?>
 <div class="site-index">
     <h4 class="text-center">
-        <?= Yii::t('app', 'Urganch davlat universitetining 
-2021/2022 o`quv yili qabul uchun xorijiy oliy ta`lim muassasalari bilan xamkorlikda qo`shma ta`lim dasturlari asosida kadrlar tayyorlash bo`yicha to`lov-kontrakt asosida qabul ko`rsatkichlari
-') ?>
-    </h4>
+        <?= Yii::t('app', "Urganch davlat universitetining 
+2022-2023-o'quv yili uchun xorijiy abituriyentlar qabuli
+") ?> </h4>
+        <p><span style="text-align: right;font-family:times new roman,times,serif"><strong><span style="font-size:16px">1 . Pasport va xalqaro&nbsp;pasport;<br />
+2. Oʼzbekistonda yashash guvohnomasi nusxasi (vid na jitelstvo) - agar mavjud boʼlsa;<br />
+3. Xorijiy fuqaroning oʼrta yoki oʼrta maxsus, kasb-hunar maʼlumotini tasdiqlovchi shahodatnoma/diplom/ sertifikatning oʼzbek yoki rus tiliga tarjima qilingan va notarius tomonidan tasdiqlangan hujjat;<br />
+4. Tibbiy maʼlumotnoma (086 shakli)<br />
+5. Fotosurat (3,5-4,5); (alohida elektron shakli)<br />
+Talab etiladigan hujjatlarning elektoron shakli(PDF shaklida, koʼrsatilgan hujjatlar tartibi raqami saqlangan 1 fayl boʼlgan holda) quyidagi Ariza tugmasi orqali Ariza qoldirishingiz mumkin</span></strong></span></p>
+
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'pager' => ['class' => '\yii\bootstrap4\LinkPager'],
@@ -34,54 +40,54 @@ $this->title = Yii::t('app', 'UrSU Vacancy');
         ],
         'columns' => [
             ['class' => SerialColumn::class],
-            [
-                'label' => 'Shifr',
-                'attribute' => 'code',
-            ],
-            [
-                'label' => 'Ta`lim yo`nalishlari va mutaxassisliklari nomi',
-                'attribute' => 'direction',
-            ],
-            [
-                'label' => 'Xamkor xorijiy oliy ta`lim muassasasi nomi',
-                'attribute' => 'university',
-            ],
-            [
-                'label' => 'To`lov-kantrakti asosida qabul ko`rsatkichlari',
-                'attribute' => 'quota'
-            ],
-            [
-                'label' => 'Ta`lim shakli',
-                'attribute' => 'form_education',
-            ],
-            [
-                'label' => 'Ta`lim tili',
-                'attribute' => 'edu_lang',
-            ],
-            [
-                'label' => 'O`qish muddati (OTM + xamkor OTM,yil)',
-                'attribute' => 'duration',
-            ],
-            [
-                'label' => 'Ta`lim turi',
-                'value' =>function(Section $model){
-                    if ($model->type ==='bakalavr'){
-                        return 'Bakalavriyat';
-                    }
-                    if ($model->type === 'magistr'){
-                        return 'Magistratura';
-                    }
-                }
-            ],
-
-            [
-                'label' => 'Ko`rish',
-                'format' => 'html',
-                'value' => function (Section $model, $key, $index) {
-                    return Html::a('Ariza', ['site/send-resume', 'id' => $model->id], ['class' => 'btn btn-info']);
-                }
-
-            ],
+//            [
+//                'label' => 'Shifr',
+//                'attribute' => 'code',
+//            ],
+//            [
+//                'label' => 'Ta`lim yo`nalishlari va mutaxassisliklari nomi',
+//                'attribute' => 'direction',
+//            ],
+//            [
+//                'label' => 'Xamkor xorijiy oliy ta`lim muassasasi nomi',
+//                'attribute' => 'university',
+//            ],
+//            [
+//                'label' => 'To`lov-kantrakti asosida qabul ko`rsatkichlari',
+//                'attribute' => 'quota'
+//            ],
+//            [
+//                'label' => 'Ta`lim shakli',
+//                'attribute' => 'form_education',
+//            ],
+//            [
+//                'label' => 'Ta`lim tili',
+//                'attribute' => 'edu_lang',
+//            ],
+//            [
+//                'label' => 'O`qish muddati (OTM + xamkor OTM,yil)',
+//                'attribute' => 'duration',
+//            ],
+//            [
+//                'label' => 'Ta`lim turi',
+//                'value' =>function(Section $model){
+//                    if ($model->type ==='bakalavr'){
+//                        return 'Bakalavriyat';
+//                    }
+//                    if ($model->type === 'magistr'){
+//                        return 'Magistratura';
+//                    }
+//                }
+//            ],
+//
+//            [
+//                'label' => 'Ko`rish',
+//                'format' => 'html',
+//                'value' => function (Section $model, $key, $index) {
+//                    return Html::a('Ariza', ['site/send-resume', 'id' => $model->id], ['class' => 'btn btn-info']);
+//                }
+//
+//            ],
             [
                 'label' => 'Ko`rish (Turkmaniston fuqarolari uchun)',
                 'format' => 'html',
