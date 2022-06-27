5ce482897181de12e5a7de3f48f89746902e6e02
DilshodDev <allashkurovdilshod@gmail.com>
2022-06-27 10:20:38 +0500

    Ursu vacansy update Xalqaro qabul

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -18,14 +18,14 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\widgets\MaskedInput;
 
-$this->title = Yii::t('app', 'UrSU Vacancy');
+$this->title = Yii::t('app', 'UrSU Xalqaro qabul');
 ?>
 <div class="site-index">
-    <h4 class="text-center">
+    <h4 style="margin-top: 3rem;" class="text-center">
         <?= Yii::t('app', "Urganch davlat universitetining 
 2022-2023-o'quv yili uchun xorijiy fuqarolar qabuli
 ") ?> </h4>
-    <p><span style="text-align: right;font-family:times new roman,times,serif"><strong><span style="font-size:16px">1 . Pasport va xalqaro&nbsp;pasport;<br />
+    <p style="margin-top: 2rem;"><span style="text-align: right;font-family:times new roman,times,serif"><strong><span style="font-size:16px">1 . Pasport va xalqaro&nbsp;pasport;<br />
 2. Oʼzbekistonda yashash guvohnomasi nusxasi (vid na jitelstvo) - agar mavjud boʼlsa;<br />
 3. Xorijiy fuqaroning oʼrta yoki oʼrta maxsus, kasb-hunar maʼlumotini tasdiqlovchi shahodatnoma/diplom/ sertifikatning oʼzbek yoki rus tiliga tarjima qilingan va notarius tomonidan tasdiqlangan hujjat;<br />
 4. Tibbiy maʼlumotnoma (086 shakli)<br />
